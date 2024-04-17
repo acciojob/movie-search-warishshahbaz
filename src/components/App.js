c513b3100b100ac554d4ed191bba936aee10fa15
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-17 14:52:38 +0530

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import MovieSearch from "./SearchMovies";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <MovieSearch />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SearchMovies.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const MovieSearch = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const API_KEY = "99eb9fd1";
+  const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`${API_URL}&s=${searchQuery}`);
+      if (response.data.Response === "True") {
+        setSearchResults(response.data.Search);
+        setErrorMessage("");
+      } else {
+        setSearchResults([]);
+        setErrorMessage("Invalid movie name. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setSearchResults([]);
+      setErrorMessage("Something went wrong. Please try again later.");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search for a movie..."
+      />
+      <button onClick={handleSearch}>Search</button>
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      <div className="search-results">
+        {searchResults.map((movie) => (
+          <div key={movie.imdbID} className="movie">
+            <img src={movie.Poster} alt={movie.Title} />
+            <div>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieSearch;
